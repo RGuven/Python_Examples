028e641d6848961a0899044137f6bb0f7f727c2a
RGuven <37475770+RGuven@users.noreply.github.com>
2019-03-28 08:29:02 +0300

    Update SayıBulmaOyunu.py

--- SayıBulmaOyunu.py
+++ SayıBulmaOyunu.py
@@ -1,2 +1,22 @@
+
+
+import random
+
+bs=random.randint(1,99)
+tahmin=-1
+s=0
+print(bs)
+while (tahmin != bs):
+    tahmin=int(input("Sayı Giriniz: "))
+    s=s+1
+    
+    if (tahmin > bs):
+        print("Daha Küçük Sayı Giriniz.")
+        
+    if (tahmin < bs):
+        print("Daha Büyük Sayı Giriniz.")
+        
+print("Doğru Bildiniz Tahmininiz:",tahmin)
+=======
 print("""SAYI BULMA OYUNUNA HOŞGELDİNİZ.OYUNUNDA RASTGELE 
       DEĞERLER GİREREK DOĞRU MU YANLIŞ MI OLDUĞUNU BULMAYA ÇALIŞACAĞIZ""")
