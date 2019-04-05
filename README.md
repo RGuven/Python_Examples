fa625506b54a627f33f68843dea07297169cb720
Ramazan <rguven887@gmail.com>
2019-04-05 23:19:21 +0300

    Sayı bulma oyunu ilk Hali

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Python_Examples
+<<<<<<< HEAD
+<<<<<<< HEAD
+Python ile yazılan örnek kodları içeren bu projelere ekleme yapıp beraberce geliştirelim.
+=======
+The world of my code 
+>>>>>>> 93231048b88d5876f0e314539cc93eca491bacbe
+=======
+The world of my code 
+>>>>>>> 93231048b88d5876f0e314539cc93eca491bacbe
--- SayıBulmaOyunu.py
+++ SayıBulmaOyunu.py
@@ -1,1 +1,41 @@
-print("Başlıyoruz...........")
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+print("""SAYI BULMA OYUNUNA HOŞGELDİNİZ.SAYI TAHMİN EDEREK DOĞRU CEVABI BULUNUZ""")
+=======
+print("""SAYI BULMA OYUNUNA HOŞGELDİNİZ.OYUNUNDA RASTGELE 
+      DEĞERLER GİREREK DOĞRU MU YANLIŞ MI OLDUĞUNU BULMAYA ÇALIŞACAĞIZ""")
+
+>>>>>>> 93231048b88d5876f0e314539cc93eca491bacbe
+=======
+print("""SAYI BULMA OYUNUNA HOŞGELDİNİZ.OYUNUNDA RASTGELE 
+      DEĞERLER GİREREK DOĞRU MU YANLIŞ MI OLDUĞUNU BULMAYA ÇALIŞACAĞIZ""")
+
+>>>>>>> 93231048b88d5876f0e314539cc93eca491bacbe
+
+import random
+
+bs=random.randint(1,99)
+tahmin=-1
+s=0
+print(bs)
+while (tahmin != bs):
+    tahmin=int(input("Sayı Giriniz: "))
+    s=s+1
+    
+    if (tahmin > bs):
+        print("Daha Küçük Sayı Giriniz.")
+        
+    if (tahmin < bs):
+        print("Daha Büyük Sayı Giriniz.")
+        
+print("Doğru Bildiniz Tahmininiz:",tahmin)
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> 93231048b88d5876f0e314539cc93eca491bacbe
+=======
+
+>>>>>>> 93231048b88d5876f0e314539cc93eca491bacbe
